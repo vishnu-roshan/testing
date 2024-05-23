ccfbbdaf078c51d7a2ec30f8c28ae9239669fa4b
vishnu-roshan <61075894+vishnu-roshan@users.noreply.github.com>
2024-05-23 17:12:25 +0530

    Update run_smoke_tests.sh

--- Testsigma/run_smoke_tests.sh
+++ Testsigma/run_smoke_tests.sh
@@ -20,8 +20,8 @@
 # For example, we are using $(date +"%Y%m%d%H%M") to use current data and time as build number.
 #
 #********START USER_INPUTS*********
-TESTSIGMA_API_KEY=eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMDM5ZDE0NC0zOWM0LTQ2MGMtODVkOS0zNjU1MDkxNjI4OWQiLCJkb21haW4iOiJ0ZXN0c2lnbWF0ZWNoLmNvbSIsInRlbmFudElkIjoyODE3fQ.lpw5KELasebWomb0llXQmxpZuCII-82VPwqzDO63xC7sYunu7vAG5KQNfkX6-JuuQxyIjHM96n_bU5GZz3t8tA
-TESTSIGMA_TEST_PLAN_ID=802
+TESTSIGMA_API_KEY=eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxZjVhZjc1Yy0wODliLTQ3ZTQtOTU4MS0wZmNjYjJhMzc4YmIiLCJkb21haW4iOiJ0ZXN0c2lnbWF0ZWNoLmNvbSIsInRlbmFudElkIjoyODE3fQ.mL_XnxTUY_G_vsFvFmVSSmogtQhGSWyzkRKxDYRBmxLvAnZY3PMAVJ2Q2qkmcuFE-IZJ_Wra8i0968D7QEPJBg
+TESTSIGMA_TEST_PLAN_ID=12955
 MAX_WAIT_TIME_FOR_SCRIPT_TO_EXIT=10
 JUNIT_REPORT_FILE_PATH=./junit-report-$(date +"%Y%m%d%H%M").xml
 RUNTIME_DATA_INPUT="url=https://the-internet.herokuapp.com/login,test=1221"
